3a5bfe9c1e3e79b7d94894510f61979847f090f6
Jirayu Roungsuriyaviboon <odlho4869@gmail.com>
2019-04-24 13:17:07 +0700

    add AmplitudeTracker

---
+++ src/tracker/amplitude.js
@@ -0,0 +1,54 @@
+import createDebug from "debug"
+import { BaseTracker } from "./base"
+
+const debug = createDebug("rentspree-tracker:amplitude")
+
+/**
+ * The class for Amplitude tracker
+ * @param {String} options.amplitudeSDK the amplitude SDK
+ * @param {String} options.apiKey the api key of amplitude
+ * @extends BaseTracker
+ */
+export class AmplitudeTracker extends BaseTracker {
+  constructor(options) {
+    super(options)
+    this.amplitudeSDK = options.amplitudeSDK
+    this.apiKey = options.apiKey
+    if (this.amplitudeSDK && this.apiKey) {
+      debug("initialize Amplitude for Instance %o", options.amplitudeSDK)
+      debug("initialize Amplitude Instance for API KEY %o", options.apiKey)
+      this.getTracker().init(this.apiKey)
+    }
+  }
+  /**
+   * Static method for getting the amplitude tracker
+   * @returns {Function | Proxy} the amplitude instance if it exist,
+   * this method will return Proxy to avoid error
+   * @static
+   */
+  getTracker() {
+    if (this.amplitudeSDK) {
+      debug("return amplitude instance")
+      return this.amplitudeSDK.getInstance()
+    }
+    debug("return proxy object for now")
+    return new Proxy(
+      {},
+      {
+        get: () => () => {}
+      }
+    )
+  }
+
+  /**
+   * Identify the user by calling `setUserId(userId)`
+   * the `userId` is a return from `options.mapUserProfile(profile)`
+   * @param {Object} profile the profile object
+   */
+  identifyUser(profile) {
+    debug("identify user %o", profile)
+    const mappedProfile = this.mapUserProfile(profile)
+    debug("amplitude.setUserId(%s)", mappedProfile)
+    this.getTracker().setUserId(mappedProfile)
+  }
+}
---
+++ test/tracker/amplitude.test.js
@@ -0,0 +1,95 @@
+import { AmplitudeTracker } from "../../src/tracker/amplitude"
+
+describe("AmplitudeTracker", () => {
+  describe("constructor", () => {
+    it("should set apiKey from option", () => {
+      const amplitudeTracker = new AmplitudeTracker({ apiKey: "hello" })
+      expect(amplitudeTracker.apiKey).toEqual("hello")
+    })
+    it("should set amplitudeSDK from option", () => {
+      const mockGetInstance = { getInstance: jest.fn() }
+      const amplitudeTracker = new AmplitudeTracker({
+        amplitudeSDK: mockGetInstance
+      })
+      expect(amplitudeTracker.amplitudeSDK).toEqual(mockGetInstance)
+    })
+    it("should init tracker if amplitudeSDK and apiKey is exist", () => {
+      const initTracker = jest.fn()
+      const mockAmplitudeSDK = {
+        getInstance: () => ({
+          init: initTracker
+        })
+      }
+      const apiKey = "apiKey"
+      const tracker = new AmplitudeTracker({
+        apiKey,
+        amplitudeSDK: mockAmplitudeSDK
+      })
+      expect(initTracker).toBeCalledWith(apiKey)
+    })
+    it("should not init tracker if amplitudeSDK or apiKey is not exist", () => {
+      const initTracker = jest.fn()
+      const mockAmplitudeSDK = {
+        getInstance: () => ({
+          init: initTracker
+        })
+      }
+      const apiKey = "apiKey"
+      const tracker = new AmplitudeTracker({
+        amplitudeSDK: mockAmplitudeSDK
+      })
+      expect(initTracker).not.toBeCalledWith(apiKey)
+    })
+  })
+  describe("getTracker", () => {
+    it("should return amplitude instance if it has one", () => {
+      const apiKey = "apiKey"
+      const initTracker = jest.fn()
+      const mockAmplitudeSDK = {
+        getInstance: () => ({
+          init: initTracker
+        })
+      }
+      const amplitudeTracker = new AmplitudeTracker({
+        apiKey,
+        amplitudeSDK: mockAmplitudeSDK
+      })
+      expect(amplitudeTracker.getTracker()).toEqual(
+        mockAmplitudeSDK.getInstance()
+      )
+    })
+    it("should return a proxy if amplitude is not available", () => {
+      const amplitudeTracker = new AmplitudeTracker({
+        apiKey: "apiKey"
+      })
+      const proxy = amplitudeTracker.getTracker()
+      // if the returned object is a proxy it will not throw error on calling
+      // some undefined function
+      expect(proxy.someFunction).not.toThrow()
+    })
+  })
+  describe("tracking function", () => {
+    const apiKey = "the-trackingId"
+    const setUserIdMock = jest.fn()
+    const mockAmplitudeSDK = {
+      getInstance: () => ({
+        init: jest.fn(),
+        setUserId: setUserIdMock
+      })
+    }
+    const amplitudeTracker = new AmplitudeTracker({
+      apiKey,
+      amplitudeSDK: mockAmplitudeSDK,
+      mapUserProfile: profile => profile.id
+    })
+    describe("identifyUser", () => {
+      it("should call tracker config", () => {
+        const profile = {
+          id: "userId"
+        }
+        amplitudeTracker.identifyUser(profile)
+        expect(setUserIdMock).toBeCalledWith(profile.id)
+      })
+    })
+  })
+})
